ec56e8cdde11c5fc2128dbaddaa8b3208eeeaff0
Verruckte <Soulkillerdeath@gmail.com>
2021-08-03 16:55:33 +0300

    dz6

--- app/src/main/java/com/example/pictureoftheday/ui/picture/BottomNavigationDrawerFragment.kt
+++ app/src/main/java/com/example/pictureoftheday/ui/picture/BottomNavigationDrawerFragment.kt
@@ -1,11 +1,13 @@
 package com.example.pictureoftheday.ui.picture
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.pictureoftheday.R
+import com.example.pictureoftheday.ui.recycler_activity.RecyclerActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.navigation.NavigationView
 
@@ -38,6 +40,14 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
                     context, R.string.screen_2,
                     Toast.LENGTH_SHORT
                 ).show()
+                R.id.navigation_three -> activity?.let {
+                    startActivity(
+                            Intent(
+                                    it,
+                                    RecyclerActivity::class.java
+                            )
+                    )
+                }
             }
             true
         }
